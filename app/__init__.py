b2208d1849d69d61c1bd1339866ffe221c303fa6
ramkumar142002 <ramkumarsv1401@gmail.com>
2024-09-25 22:56:06 +0530

    Timezone using moment.js

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +14,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 login.login_view = "login"
 
