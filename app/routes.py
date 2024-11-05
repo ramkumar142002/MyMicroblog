10723fcad02fb21407a63d890e50e55950595886
ramkumar142002 <ramkumarsv1401@gmail.com>
2024-11-05 23:57:19 +0530

    i18n translations

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,10 @@ from werkzeug.urls import url_parse
 from app import db
 from app.forms import ResetPasswordRequestForm
 from app.email import send_password_reset_email
+from guess_language import guess_language
+from datetime import datetime
+from flask import g
+from flask_babel import get_locale
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -15,7 +19,11 @@ from app.email import send_password_reset_email
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user,
+                    language=language)
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')
@@ -184,3 +192,10 @@ def reset_password(token):
         flash('Your password has been reset.')
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+        g.locale = str(get_locale())
