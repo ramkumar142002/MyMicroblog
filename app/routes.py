14ef523a9cdde3332f5cfb328c92be19365cd22a
Ramkumar <ramkumarsv1401@gmail.com>
2024-12-30 01:04:22 +0530

    requirements

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from app import app
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Post
-from werkzeug.urls import url_parse
+from urllib.parse import urlparse
 from app import db
 from app.forms import ResetPasswordRequestForm
 from app.email import send_password_reset_email
@@ -52,7 +52,7 @@ def login():
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get("next")
-        if not next_page or url_parse(next_page).netloc != "":
+        if not next_page or urlparse(next_page).netloc != "":
             next_page = url_for("index")
         return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
